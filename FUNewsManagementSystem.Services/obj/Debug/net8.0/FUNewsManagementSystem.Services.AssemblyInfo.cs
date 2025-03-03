f05f63f302101f376d7015fcaf173554b1b40ba7
PhatNguyenDev <139215308+PhatNHDev03@users.noreply.github.com>
2025-03-03 13:51:20 +0700

    kho

--- FUNewsManagementSystem.Services/obj/Debug/net8.0/FUNewsManagementSystem.Services.AssemblyInfo.cs
+++ FUNewsManagementSystem.Services/obj/Debug/net8.0/FUNewsManagementSystem.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FUNewsManagementSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f1636188a8434e3c5ab74715f63b71ffefdc2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6d9915946a57a69d50d39ffe926bf6495a0796")]
 [assembly: System.Reflection.AssemblyProductAttribute("FUNewsManagementSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FUNewsManagementSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
